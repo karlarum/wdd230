3c810586e53b0dd6147aa2c6ff9f6bee55615455
karlarum <131808949+karlarum@users.noreply.github.com>
2024-05-03 19:50:51 -0500

    partial work done for design principles

---
+++ lesson02/scripts/design-get-dates.js
@@ -0,0 +1,5 @@
+// copyright year
+document.querySelector('#year').textContent = new Date().getFullYear();
+
+//last modified
+document.querySelector('#lastModified').textContent = "Last Modification: " + document.lastModified;
