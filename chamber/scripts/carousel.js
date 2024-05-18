e426c340e9528e0aa44fa6168e89a33d3863f57b
karlarum <131808949+karlarum@users.noreply.github.com>
2024-05-18 02:16:37 -0500

    assignment changes

---
+++ chamber/scripts/carousel.js
@@ -0,0 +1,19 @@
+// const carouselReviews = document.querySelector('.carousel-reviews');
+// const reviews = document.querySelector('.carousel-review');
+// const prev = document.getElementById('prev');
+// const next = document.getElementById('next');
+
+// let counter = 0;
+// const size = reviews[0].clientWidth;
+
+// next.addEventListener('click', () => {
+//     if (counter >= reviews.length - 1) return;
+//     counter++;
+//     carouselReviews.style.transform = `translateX(-${counter * reviewWidth}px)`;
+// });
+
+// prev.addEventListener('click', () => {
+//     if (counter <= 0) return;
+//     counter--;
+//     carouselReviews.style.transform = `translateX(-${counter * reviewWidth}px)`;
+// });
---
+++ chamber/scripts/hero.js
@@ -0,0 +1,3 @@
+document.getElementById('hero-button').addEventListener('click', function () {
+    window.location.href = ''; //add url here
+})
