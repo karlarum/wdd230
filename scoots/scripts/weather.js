2a22bd0e83e00d2b7c628d06847f2e426e4cdbcd
karlarum <131808949+karlarum@users.noreply.github.com>
2024-07-12 15:57:09 -0500

    index page update

---
+++ scoots/scripts/weather.js
@@ -0,0 +1,54 @@
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=30.481253&lon=-86.442100&units=imperial&appid=9a2fcfbaf1ae7d3e201a68db176df4cd';
+const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=30.481253&lon=-86.442100&units=imperial&appid=9a2fcfbaf1ae7d3e201a68db176df4cd';
+
+// handle errors using a block function
+async function apiFetch() {
+    try {
+        // fetch current weather
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+
+        // fetch forecast
+        const forecastResponse = await fetch(forecastUrl);
+        if (forecastResponse.ok) {
+            const forecastData = await forecastResponse.json();
+            displayForecast(forecastData);
+        } else {
+            throw Error(await forecastResponse.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// display results
+function displayResults(data) {
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    desc = desc.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.innerHTML = `${Math.round(data.main.temp)}&deg;F - ` + desc;
+}
+
+function displayForecast(data) {
+    for (let i = 0; i < 3; i++) {
+        const forecast = data.list[i * 8];
+        const iconsrc = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
+        let desc = forecast.weather[0].description;
+        desc = desc.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+        document.getElementById(`forecast-icon${i + 1}`).setAttribute('src', iconsrc);
+        document.getElementById(`forecast-icon${i + 1}`).setAttribute('alt', desc);
+        document.getElementById(`forecast-desc${i + 1}`).innerHTML = `${Math.round(forecast.main.temp)}°F - ${desc}`;
+    }
+}
+
+apiFetch();
